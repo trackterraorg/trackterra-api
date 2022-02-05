b33685f1dd5a252b11a70159af633cb65753af44
Rebaz Saleh <rebaz88.software@gmail.com>
2022-02-05 23:18:10 +0300

    fix cors

--- libs/common/src/utils/cors-config.util.ts
+++ libs/common/src/utils/cors-config.util.ts
@@ -2,15 +2,22 @@ import { CorsOptions } from '@nestjs/common/interfaces/external/cors-options.int
 
 const whitelist = [
   '/trackterra.org$/',
-  'http://localhost:3000',
+  'http://localhost:30000',
   'http://localhost:4000',
   'http://localhost:80',
   'http://localhost:8080',
 ];
 
 export const corsOptions: CorsOptions = {
-  origin: '*',
-  // preflightContinue: true,
+  origin: function (origin, callback) {
+    if (whitelist.includes(origin)) {
+      callback(null, true)
+    } else {
+      callback(new Error('Not allowed by CORS'))
+    }
+  },
+  allowedHeaders: 'X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept, Observe',
+  methods: "GET,PUT,POST,DELETE,UPDATE,OPTIONS",
   credentials: true,
   optionsSuccessStatus: 200,
 };
