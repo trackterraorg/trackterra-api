171003410c96b9388bc64cf91616b9310adde0bd
Rebaz Saleh <rebaz88.software@gmail.com>
2022-02-06 00:11:23 +0300

    fix(cors): allow by cors by regex

--- libs/common/src/utils/cors-config.util.ts
+++ libs/common/src/utils/cors-config.util.ts
@@ -1,7 +1,7 @@
 import { CorsOptions } from '@nestjs/common/interfaces/external/cors-options.interface';
 
 const whitelist = [
-  'https://trackterra.org',
+  '/trackterra.org$/',
   'http://localhost:3000',
   'http://localhost:4000',
   'http://localhost:80',
@@ -9,13 +9,7 @@ const whitelist = [
 ];
 
 export const corsOptions: CorsOptions = {
-  origin: function (origin, callback) {
-    if (whitelist.includes(origin)) {
-      callback(null, true)
-    } else {
-      callback(new Error('Not allowed by CORS'))
-    }
-  },
+  origin: whitelist,
   allowedHeaders: 'X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept, Observe',
   methods: "GET,PUT,POST,DELETE,UPDATE,OPTIONS",
   credentials: true,
