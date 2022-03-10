afe691f2b0bd35a7b49e713cc5f5c1d798efe2a3
Rebaz Saleh <rebaz88.software@gmail.com>
2022-03-10 08:04:19 +0300

    fix edge repay

--- libs/parser/src/parser/processers/edge.ts
+++ libs/parser/src/parser/processers/edge.ts
@@ -34,34 +34,6 @@ export class EdgeDepositCollateral implements IParser {
   }
 }
 
-export class EdgeRepay implements IParser {
-  process(args: ParserProcessArgs): IParsedTx[] {
-    const { walletAddress, contractActions } = args;
-
-    const repayActions = contractActions.repay.map((cA: any) => {
-      return {
-        contract: cA.contract,
-        sender: cA.owner,
-        recipient: cA.contract,
-        amount: {
-          amount: cA.amount,
-          token: cA.underlying,
-        }
-      }
-    });
-
-    const repayTx = (new TransferEngine()).process({
-      ...args,
-      contractActions: undefined,
-      transferActions: repayActions,
-    });
-
-    return repayTx;
-  }
-}
-
-
 export const EdgeProtocol = {
   EdgeDepositCollateral,
-  EdgeRepay,
 };
