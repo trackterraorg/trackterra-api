b116132bff9688bf8de17db1b15a3997056f5a63
Rebaz Saleh <rebaz88.software@gmail.com>
2022-03-13 14:45:13 +0300

    fix nft deposit and purchase in one parser

--- libs/parser/src/parser/processers/nft.ts
+++ libs/parser/src/parser/processers/nft.ts
@@ -12,7 +12,7 @@ export class PurchaseNFT implements IParser {
 
     const { walletAddress, contractActions, txType } = args;
 
-    return contractActions.execute_orders.map((order) : IParsedTx => {
+    return contractActions?.execute_orders.map((order) : IParsedTx => {
       const parsedOrder = JSON.parse(Base64.decode(order.order as unknown as string))?.order;
 
       const makerAsset = parsedOrder.maker_asset;
@@ -65,7 +65,69 @@ export class TransferNFT implements IParser {
   
 }
 
+export class RandomEarthDeposit implements IParser {
+
+  process(args: ParserProcessArgs): IParsedTx[] {
+
+    const { walletAddress, contractActions, transferActions, allEvents} = args;
+
+
+    const depositTx = (new TransferEngine()).process({
+      ...args,
+      contractActions: undefined,
+      transferActions: transferActions,
+    });
+
+    const purchaseContractActions = allEvents.find((cA: any) => {
+        return Object.keys(cA.contractActions).includes('execute_orders');
+    })?.contractActions;
+
+    let purchaseTx = [];
+    if (purchaseContractActions) {
+      purchaseTx = (new PurchaseNFT()).process({
+        ...args,
+        contractActions: purchaseContractActions,
+        transferActions: undefined,
+        txType: {
+          ...args.txType,
+          description: 'Random earth purchase',
+          tag: TxTag.Swap,
+        }
+      });
+
+      const depositAmount: number = _.first(depositTx).sentAmount as unknown as number;
+      _.first(depositTx).sentAmount = `${(depositAmount - _.first(purchaseTx).sentAmount)}`;
+    }
+
+    return depositTx.concat(purchaseTx);
+    // console.dir({
+    //   'hererr': 'herrrrrrrrrrrrrrrr'
+    // }, {depth: 'null'});
+    // const { walletAddress, txType, contractActions } = args;
+
+    // const key = _(['transfer_nft', 'send_nft']).intersection(Object.keys(contractActions)).first();
+
+    // const transferActions = contractActions[key].map((cA: any) => {
+    //   const amount: IAmount = {
+    //     amount: '1',
+    //     token: cA.contract,
+    //   }
+
+    //   cA.amount = amount;
+    //   return cA;
+    // });
+
+    // return (new TransferEngine()).process({
+    //   ...args, 
+    //   contractActions: undefined,
+    //   transferActions, 
+    // });
+  }
+  
+}
+
 export const NFTParsers = {
   PurchaseNFT,
   TransferNFT,
+  RandomEarthDeposit,
 };
