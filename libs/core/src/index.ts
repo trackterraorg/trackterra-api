19d2425b59e1d0d12c76bf0507382480f5c9fed3
Rebaz Saleh <rebaz88.software@gmail.com>
2022-04-24 21:21:29 +0300

    remove microservice setup

--- libs/core/src/index.ts
+++ libs/core/src/index.ts
@@ -3,6 +3,5 @@ export * from './decorators';
 export * from './modules';
 export * from './services';
 export * from './scalers';
-export * from './setup';
 export * from './helpers';
 export * from './graphql';
