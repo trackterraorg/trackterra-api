bfd06dc1803ff4dc7a7e72fa403afb9dbeffc27a
Rebaz Saleh <rebaz88.software@gmail.com>
2022-04-24 09:46:29 +0300

    remove seed module

--- app/src/app.module.ts
+++ app/src/app.module.ts
@@ -1,16 +1,13 @@
-import { CacheModule, Module } from '@nestjs/common';
-import { GraphQLModule } from '@nestjs/graphql';
+import { Module } from '@nestjs/common';
 import { CookieSerializer } from '@trackterra/common';
 import { ServiceRegistryModule } from '@trackterra/core';
 import { AppService } from './app.service';
-import { SeedModule } from './seed.module';
 import { CurrenciesModule } from './currencies/currencies.module';
 import { WalletsModule } from './wallets/wallets.module';
 import { ParserModule } from './parser/parser.module';
 
 @Module({
   imports: [
-    SeedModule,
     ServiceRegistryModule,
     WalletsModule,
     ParserModule,
